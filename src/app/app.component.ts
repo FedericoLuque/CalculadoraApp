655920bb54152f32bb62aa8d918b319a0163ea25
Federico <fluque87@gmail.com>
2024-09-18 21:13:04 +0200

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'CalculadoraApp';
+  title = 'Aplicación de calculadora';
+
+  operandoA : number = NaN;
+
+  operandoB : number = NaN;
+
+  resultado : number = 0;
+
+
+  suma():void{
+
+    this.resultado = this.operandoA + this.operandoB;
+
+  }
+
+  resta():void{
+
+    this.resultado = this.operandoA - this.operandoB;
+
+  }
+
+  multiplica():void{
+  
+    this.resultado = this.operandoA * this.operandoB
+
+  }
+
+  divide():void{
+
+  this.resultado = this.operandoA / this.operandoB;
+
+  }
+
 }
